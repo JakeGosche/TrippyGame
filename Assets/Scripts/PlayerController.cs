08d418de5d6307df0960244603dde6fbceeeb5e8
TheTKPizza <rasseljg@aol.com>
2020-11-03 22:39:53 +0100

    Minor audio tests

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class EventTest1 : MonoBehaviour
-{
-    void Start()
-    {
-        EventTrigger trigger = GetComponent<EventTrigger>();
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = EventTriggerType.PointerDown;
-        entry.callback.AddListener((data) => { OnPointerDownDelegate((PointerEventData)data); });
-        trigger.triggers.Add(entry);
-    }
-
-    public void OnPointerDownDelegate(PointerEventData data)
-    {
-        Debug.Log("OnPointerDownDelegate called.");
-    }
-}
-
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
