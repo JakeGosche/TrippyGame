a6e7ecf570a5b6c649334961cc2059e1e3c23554
TheTKPizza <rasseljg@aol.com>
2020-11-03 13:15:44 +0100

    Restore Unity Integration Exper

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,18 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FLUTE = 1653871456U;
+        static const AkUniqueID PLAY_FLUTE_END = 129809062U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID FLUTE = 1167860281U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
